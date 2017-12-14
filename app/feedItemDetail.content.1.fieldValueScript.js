0f6550a2a7a64ff95f438517673d17ff45f558e7
nickolanack <nickblackwell82@gmail.com>
2017-12-14 10:41:57 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/feedItemDetail.content.1.fieldValueScript.js
+++ app/feedItemDetail.content.1.fieldValueScript.js
@@ -1,1 +1,9 @@
+
+
+if(AppClient.getUserType()=="guest"){
+    return "You are not logged in"
+}
+
+
+
 return (item.getName?item.getName():'{name}')+' - '+(item.getDescription?item.getDescription():'{description}')
