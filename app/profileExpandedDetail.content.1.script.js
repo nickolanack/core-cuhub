d8f43136a222f4f1ac2f757394362dd99d7b8b6b
nickolanack <nickblackwell82@gmail.com>
2017-12-14 10:53:47 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/profileExpandedDetail.content.1.script.js
@@ -0,0 +1,7 @@
+return new ModalFormButtonModule(application, AppClient, {
+    "label":"Create Project",
+    "formName":"createEvent",
+    "formOptions":{
+        "template":"form"
+    }
+})
---
+++ app/profileExpandedDetail.content.2.script.js
@@ -0,0 +1,7 @@
+return new ModalFormButtonModule(application, AppClient, {
+    "label":"Add Personal Connection",
+    "formName":"createConnection",
+    "formOptions":{
+        "template":"form"
+    }
+})
