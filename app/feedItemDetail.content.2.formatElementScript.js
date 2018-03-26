aec3a4352ae78dd53681fc3769a7031e9a9cba67
nickolanack <nickblackwell82@gmail.com>
2018-03-26 13:18:21 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/feedItemDetail.content.2.formatElementScript.js
+++ app/feedItemDetail.content.2.formatElementScript.js
@@ -1,4 +1,4 @@
-el.addClass('feed-item-label count-posts');
+el.addClass('feed-item-label count-posts loading');
 
 (new AjaxControlQuery(CoreAjaxUrlRoot, 'discussion_metadata', {
 		                'plugin': "Discussions",
@@ -6,7 +6,14 @@ el.addClass('feed-item-label count-posts');
 		                'item':item.getId(),
 		                'channel':'default'
 		                })).addEvent('success',function(resp){
+		                    var posts=resp.metadata.posts;
+		                    el.removeClass('loading');
+		                    if(posts>0){
+		                         valueEl.innerHTML=resp.metadata.posts;
+		                    }else{
+		                        valueEl.innerHTML=""
+		                    }
 		                    
-		                    valueEl.innerHTML=resp.metadata.posts;
+		                   
 		                    
 		                }).execute();
