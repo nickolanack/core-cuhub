9d1ced4dcd56fedec516fec1c47c0b433c0bc694
nickolanack <nickblackwell82@gmail.com>
2018-03-26 15:10:15 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/feedItemDetail.content.5.formatModuleScript.js
+++ app/feedItemDetail.content.5.formatModuleScript.js
@@ -1,6 +1,12 @@
 module.getElement().addEvent('click', function(e){
     
     e.stop();
+    module.draw();
     item.activate();
     
+});
+
+
+module.addWeakEventListener(item, 'deactivate', function(){
+    module.empty();
 });
