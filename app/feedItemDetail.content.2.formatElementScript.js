e20a978ca63a80654e2fb2b7665ca42a22b53de5
nickolanack <nickblackwell82@gmail.com>
2018-03-26 12:04:48 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/feedItemDetail.content.2.formatElementScript.js
+++ app/feedItemDetail.content.2.formatElementScript.js
@@ -4,3 +4,9 @@ el.addClass('feed-item-actions')
 EventItem.CreateActionButtons(item, application).forEach(function(b){
     el.appendChild(b);
 })
+
+el.appendChild(new Element('span', {"class":'item-toggle', events:{click:function(){
+    
+    
+
+}}}));
