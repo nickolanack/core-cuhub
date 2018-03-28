3aefe9a932a8da15b2876370c40bdf0b4ab8e2d9
Nicholas  Blackwell <nblackwe@Nicholass-Mac-mini.local>
2018-03-28 12:11:52 -0700

    add more mobile meta

--- template/plain/index.php
+++ template/plain/index.php
@@ -14,9 +14,12 @@ defined('_JEXEC') or die;
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, user-scalable=yes"/>
-		<meta name="HandheldFriendly" content="true" />
-		<meta name="apple-mobile-web-app-capable" content="YES" />
+
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, user-scalable=yes"/>
+        <meta name="HandheldFriendly" content="true" />
+        <meta name="apple-mobile-web-app-capable" content="YES" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 		<jdoc:include type="head" />
 	</head>
 	<body>
