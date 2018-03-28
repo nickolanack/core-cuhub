a7a94ca7c5e0830908aa8d3b1d07abcf57927299
nickolanack <nickblackwell82@gmail.com>
2018-03-28 11:50:20 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/mainPinnedDetail.content.1.formatListItemModuleScript.js
+++ app/mainPinnedDetail.content.1.formatListItemModuleScript.js
@@ -2,6 +2,10 @@ childView.getElement().addClass((child.getType().split('.').pop())+'-feed-item')
 childView.getElement().addClass('feed-item-'+child.getId());
 
 
+if(child.isActive()){
+   childView.getElement().addClass('active');
+});
+
 childView.addWeakEvent(child, "activate", function(){
    childView.getElement().addClass('active');
 });
