7c134f380e7c19a7c427c7e497b3d6816a78ce40
nickolanack <nickblackwell82@gmail.com>
2018-04-04 12:56:12 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/leftDetail.content.3.formatModuleScript.js
+++ app/leftDetail.content.3.formatModuleScript.js
@@ -6,4 +6,10 @@ module.addEvent('selectWord',function(tag){
 	application.getNamedValue('navigationController').navigateTo("Tags", "Main");
     
     
+});
+
+module.addEvent('addWord',function(tag){
+    
+    //if matches current tag, then highlight
+    
 })
