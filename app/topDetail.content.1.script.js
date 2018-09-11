fe0d6cc928febf6ac69c0f1a9b8ad438651680f2
nickolanack <nickblackwell82@gmail.com>
2018-09-11 08:59:29 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/topDetail.content.1.script.js
@@ -0,0 +1,10 @@
+
+
+
+var banner=new ElementModule("div", {"class":"banner-image"});
+
+
+return banner;
+//return new Asset.image("https://okmain.cms.ok.ubc.ca/wp-content/uploads/sites/26/2018/08/Back-to-School-1920x780-2.jpg");
+
+//<img src="https://okmain.cms.ok.ubc.ca/wp-content/uploads/sites/26/2018/08/Back-to-School-1920x780-2.jpg" alt="" class="hidden-phone">
