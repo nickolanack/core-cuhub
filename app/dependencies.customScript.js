a65acee96f61f0165ecbc65594f66f4ad0efc8f6
nickolanack <nickblackwell82@gmail.com>
2018-09-17 09:01:16 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/dependencies.customScript.js
+++ app/dependencies.customScript.js
@@ -1,33 +1,3 @@
-IncludeJSBlock('
-
-
-TemplateModule.SetTemplate(\'form\',\'<div><div data-template="title" class="template-title"></div><div data-template="content" class="template-content"></div><div data-template="footer" class="template-footer"></div></div>\');
-TemplateModule.SetTemplate(\'default\',\'<div data-template="content" class="template-content"></div>\');
-
-
-');
-
-/**
- * Add all views that wont be autoloaded. (any view that is rendered programatically and not by default) 
- */
-
-$projectHub=GetPlugin('ProjectHub');
-
-$projectHub->setDocumentMetadata();
-
-$projectHub->includeScripts();
-
-IncludeJSBlock('
-
-    window.FeedItemListResponse='.json_encode($feedItemList=$projectHub->listFeedItemsAjax()).';
-
-');
-
-
-$projectHub->printFeedItemLinksHtml();
-
-
-
 GetWidget('cuhubStyle')->display($targetInstance);
 GetWidget('calendarStyle')->display($targetInstance);
 if(UrlVar('tpl',false)){
