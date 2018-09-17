074e1cb3502ed4ce4e0191233e04c0979c268844
nickolanack <nickblackwell82@gmail.com>
2018-09-17 09:01:26 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/CUHub Main Application.onRenderApplictionScript.js
+++ app/CUHub Main Application.onRenderApplictionScript.js
@@ -1,5 +1,34 @@
 HtmlDocument()->META(HtmlDocument()->website(), 'base');
 
+IncludeJSBlock('
+
+
+TemplateModule.SetTemplate(\'form\',\'<div><div data-template="title" class="template-title"></div><div data-template="content" class="template-content"></div><div data-template="footer" class="template-footer"></div></div>\');
+TemplateModule.SetTemplate(\'default\',\'<div data-template="content" class="template-content"></div>\');
+
+
+');
+
+/**
+ * Add all views that wont be autoloaded. (any view that is rendered programatically and not by default) 
+ */
+
+$projectHub=GetPlugin('ProjectHub');
+
+$projectHub->setDocumentMetadata();
+
+$projectHub->includeScripts();
+
+IncludeJSBlock('
+
+    window.FeedItemListResponse='.json_encode($feedItemList=$projectHub->listFeedItemsAjax()).';
+
+');
+
+
+$projectHub->printFeedItemLinksHtml();
+
+
 
 (new \core\AsyncDisplay())->display(function()use($targetInstance){
 
