61e33f5750fc5f989f75bcb586bf4631d00fde56
nickolanack <nickblackwell82@gmail.com>
2018-10-11 11:39:29 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/headerDetail.content.1.script.js
+++ app/headerDetail.content.1.script.js
@@ -4,13 +4,13 @@ var addView=function(view){
     ul.appendChild(new Element('li',{
         html:view,
         events:{click:function(){
-            
+            application.getNamedValue('navigationController').navigateTo(view, "Main");
         }}
     }));
 }
 
 (["Events", "Projects", "Connections", "Profiles"]).forEach(function(v){
     addView(v);
-})
+});
 
 return ul;
