bb2275bff08a392e1bc6a835257c93642491bdbc
nickolanack <nickblackwell82@gmail.com>
2018-10-16 17:16:42 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/feedItemInteractionDetail User.content.1.script.js
+++
@@ -1,6 +0,0 @@
-
-return EventItem.CreateConnectionToOwnerProfileButton(item, application, <?php 
-        
-       echo json_encode(($ui=GetWidget('interfaceConfig'))->getParameter('label-for-item-owner-connect'));
-    
-        ?>);
