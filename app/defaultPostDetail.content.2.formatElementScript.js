645657a8708f16bd361a06050fe3e60186bd477c
nickolanack <nickblackwell82@gmail.com>
2018-10-16 22:13:26 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/defaultPostDetail.content.2.formatElementScript.js
+++ app/defaultPostDetail.content.2.formatElementScript.js
@@ -2,6 +2,9 @@ el.addClass('post-author')
 
 var id=item.getUserId();
 el.addClass('user-id-'+id);
+if(id<=0){
+    return;
+}
 var user=EventList.SharedInstance().getProfileForUserId(id);
 if(user){
     valueEl.appendChild(EventItem.CreateAuthorLabel(user, application))
