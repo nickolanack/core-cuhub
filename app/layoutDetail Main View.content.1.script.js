0e4066d1d1c07932bcded6a96c2488cb7d629571
nickolanack <nickblackwell82@gmail.com>
2018-10-18 14:30:52 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/layoutDetail Main View.content.1.script.js
+++ app/layoutDetail Main View.content.1.script.js
@@ -1,6 +1,13 @@
 var mod= new ElementModule("div",{"class":"intro-text"})
-
-mod.appendChild(new Element('p',{
+var p=new Element('p',{
     html:"Welcome to CUHub ... and description of current page"
-}))
+});
+mod.appendChild(p)
+
+application.getNamedValue('navigationController',function(nav){
+    nav.addEvent('navigate', function(state){
+        p.innerHTML=JSON.stringify(state);
+    });
+})
+
 return mod;
