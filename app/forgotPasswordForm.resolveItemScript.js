ce72ea29f0918e19c4a67735d29235b2f6e246ae
nickolanack <nickblackwell82@gmail.com>
2018-10-30 12:17:17 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/forgotPasswordForm.resolveItemScript.js
@@ -0,0 +1,12 @@
+var ForgotUser=new Class({
+        Extends:MockDataTypeItem
+        save:function(cb){
+            
+            callback(true);
+        }
+})
+
+
+return (new ForgotUser({
+        email:""
+			}));
