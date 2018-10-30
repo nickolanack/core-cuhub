520111b788716581e0cd56412413ab71fbb48ea2
nickolanack <nickblackwell82@gmail.com>
2018-10-30 10:39:43 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/loginForm.1.content.3.script.js
+++ app/loginForm.1.content.3.script.js
@@ -3,6 +3,6 @@ return new ElementModule('a', {
     "style":"float: right; color: #2749f8; line-height: 25px;",
     events:{click:function(e){
         e.stop();
-        wizard.displayNext();
+        wizard.displayStep(1);
     }}
 });
