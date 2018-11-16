e7f86ce2fb6d3f6e3a36475f51465987a5b87241
nickolanack <nickblackwell82@gmail.com>
2018-11-16 13:30:46 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/dependencies.customScript.js
+++ app/dependencies.customScript.js
@@ -1,18 +1,11 @@
-GetWidget('cuhubStyle')->display($targetInstance);
-GetWidget('calendarStyle')->display($targetInstance);
-
-
-GetWidget('stickyRightPanelStyle')->display($targetInstance);
-
-GetWidget('cuhubResponsiveStyle')->display($targetInstance);
-
-
-GetWidget('emptyListView')->display($targetInstance);
-GetWidget('emptyPinnedListView')->display($targetInstance);
-GetWidget('cuhubGeneratedStyle')->display($targetInstance);
-
 (new core\WidgetLoader())->displayWidgets(array(
-    
+    'cuhubStyle',
+    'calendarStyle',
+    'stickyRightPanelStyle',
+    'cuhubResponsiveStyle',
+    'emptyListView',
+    'emptyPinnedListView',
+    'cuhubGeneratedStyle',
     'guestExpandedDetail',
     'profileExpandedDetail',
     'loginForm',
@@ -21,35 +14,24 @@ GetWidget('cuhubGeneratedStyle')->display($targetInstance);
     'projectForm',
     'connectionForm',
     'connectWithUserForm',
-    'connectWithEventForm'
+    'connectWithEventForm',
+    'requestForm',
+    'profileForm',
+    'dialogForm',
+    'defaultPostDetail',
+    'mainPinnedDetail',
+    'mainSingleDetail',
+    'mainArchiveDetail',
+    'mainContactDetail',
+    'mainAboutDetail',
+    'mainCalendarDetail',
+    'contactForm',
+    'directChatForm',
+    'createItemsMenuForm',
+    'eventFeedSearchItemDetail'
     
 ), $targetInstance);
 
-
-
-GetWidget('requestForm')->display($targetInstance);
-
-GetWidget('profileForm')->display($targetInstance);
-GetWidget('dialogForm')->display($targetInstance);
-
-GetWidget('defaultPostDetail')->display($targetInstance);
-
-
-GetWidget('mainPinnedDetail')->display($targetInstance);
-GetWidget('mainSingleDetail')->display($targetInstance);
-GetWidget('mainArchiveDetail')->display($targetInstance);
-
-GetWidget('mainContactDetail')->display($targetInstance);
-GetWidget('mainAboutDetail')->display($targetInstance);
-GetWidget('mainCalendarDetail')->display($targetInstance);
-
-GetWidget('contactForm')->display($targetInstance);
-GetWidget('directChatForm')->display($targetInstance);
-
-GetWidget('createItemsMenuForm')->display($targetInstance);
-
-
-GetWidget('eventFeedSearchItemDetail')->display($targetInstance);
 
 
         $apikey=GetPlugin('Maps')->getParameter('googleMapsServerApiKey', '');
