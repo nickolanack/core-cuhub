36fab8f66938c4dba8c7944f72d0f18c9bac3065
nickolanack <nickblackwell82@gmail.com>
2018-11-22 12:01:45 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/loginForm.1.content.3.script.js
+++ app/loginForm.1.content.3.script.js
@@ -1,7 +1,7 @@
 return new ElementModule('button', {
     "html":"Register a new account",
     "style":"float: right; color: #00d200; color black;",
-    "class":"btn forgot-pwd WizardButton"
+    "class":"btn forgot-pwd WizardButton",
     events:{click:function(e){
         e.stop();
         wizard.displayStep(1);
