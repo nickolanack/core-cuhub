be47777597d6b3c1650a19405aa75d0cc66894f5
nickolanack <nickblackwell82@gmail.com>
2019-02-11 21:07:29 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/feedItemDetail.content.3.formatElementScript.js
+++ app/feedItemDetail.content.3.formatElementScript.js
@@ -1,12 +1,2 @@
-el.addClass('feed-item-actions')
 
-
-EventItem.CreateActionButtons(item, application).forEach(function(b){
-    el.appendChild(b);
-})
-
-//el.appendChild(new Element('span', {"class":'item-toggle', events:{click:function(){
-    
-    
-
-//}}}));
+CuhubDashboard.appendFeedItemActions(el, item);
