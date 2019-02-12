a49e20b43eef72bb79db18b6b3c759ea8e661017
nickolanack <nickblackwell82@gmail.com>
2019-02-11 21:25:23 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/defaultPostDetail.content.2.formatElementScript.js
+++ app/defaultPostDetail.content.2.formatElementScript.js
@@ -1,12 +1,2 @@
-el.addClass('post-author')
 
-var id=item.getUserId();
-el.addClass('user-id-'+id);
-if(id<=0){
-    return;
-}
-var user=EventList.SharedInstance().getProfileForUserId(id);
-if(user){
-    valueEl.appendChild(EventItem.CreateAuthorLabel(user, application))
-}
-
+CuhubDashboard.formatDefaultPost(item, el, valueEl);
