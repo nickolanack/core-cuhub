f1320ad53c742219260442692f6ea5ecedce6cc4
nickolanack <nickblackwell82@gmail.com>
2019-02-12 20:59:28 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/layoutDetail Main View.content.1.script.js
+++ app/layoutDetail Main View.content.1.script.js
@@ -1,14 +1,2 @@
-var mod= new ElementModule("div",{"class":"intro-text"})
-var p=new Element('p',{
-    html:"Welcome to CUHub ... and description of current page"
-});
-mod.appendChild(p)
 
-application.getNamedValue('navigationController',function(nav){
-    nav.addEvent('navigate', function(state){
-       EventList.SetPageDescription(p, state);
-    });
-    EventList.SetPageDescription(p, nav.getCurrentView());
-});
-
-return mod;
+return CuhubDashboard.getPageDescriptionModule();
